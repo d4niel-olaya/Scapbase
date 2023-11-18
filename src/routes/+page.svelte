fc9a572afb18a6821678643fd8997908cd368816
d4niel <olayajuandaniel8@gmail.com>
2023-11-18 13:57:17 -0500

    csr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    let selectionOverlay : HTMLDivElement
+    let isSelecting = false;
+    let startX : number, startY : number;
 
-<h1>Svelte</h1>
-<p>This is a svelte config</p>
+
+    onMount(() =>  
+    {
+
+        document.addEventListener('mousedown', (e) => {
+            isSelecting = true;
+            startX = e.clientX;
+            startY = e.clientY;
+            selectionOverlay.style.display = 'block';
+            selectionOverlay.style.left = startX + 'px';
+            selectionOverlay.style.top = startY + 'px';
+        });
+    
+        document.addEventListener('mousemove', (e) => {
+            if (isSelecting) {
+                const currentX = e.clientX;
+                const currentY = e.clientY;
+                const width = currentX - startX;
+                const height = currentY - startY;
+    
+                selectionOverlay.style.width = width + 'px';
+                selectionOverlay.style.height = height + 'px';
+            }
+        });
+    
+        document.addEventListener('mouseup', () => {
+            isSelecting = false;
+            selectionOverlay.style.display = 'none';
+            selectionOverlay.style.width = '0';
+            selectionOverlay.style.height = '0';
+        });
+    })
+
+</script>
+
+<div class="overlay" id="selectionOverlay" bind:this={selectionOverlay}></div>
